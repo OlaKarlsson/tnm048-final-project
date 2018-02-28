fb54344d7ec3e97bc12404a41a948ce03c60b079
Ola Karlsson <ola@olakarlsson.com>
2018-02-28 13:35:33 +0100

    Changes to tooltip

--- static/js/bar.js
+++ static/js/bar.js
@@ -3,8 +3,8 @@ function bar(imdb, bechdel){
     
     
 // set the dimensions and margins of the graph
-var div = "#stacked-bar";
-var parentWidth = $(div).parent().width();
+var tooltipDiv = "#stacked-bar";
+var parentWidth = $(tooltipDiv).parent().width();
 var margin = {top: 20, right: 20, bottom: 30, left: 40},
 width = parentWidth - margin.left - margin.right,
 height = 300 - margin.top - margin.bottom;
@@ -43,6 +43,8 @@ var newData = d3.nest()
 
 
 console.log(newData);
+//Filter the data to only have years with more than a certain number of movies
+//TODO: Possible allow for dynamic filtering, based on https://bl.ocks.org/officeofjane/9b9e606e9876e34385cc4aeab188ed73
 var filterTreshold = 20;
 var filteredData = newData.filter(function(d){
     return d.count > filterTreshold;
@@ -54,7 +56,7 @@ console.log(filteredData);
 // append the svg object to the body of the page
 // append a 'group' element to 'svg'
 // moves the 'group' element to the top left margin
-var svg = d3.select(div).append("svg")
+var svg = d3.select(tooltipDiv).append("svg")
 .attr("width", width + margin.left + margin.right)
 .attr("height", height + margin.top + margin.bottom)
 .append("g")
@@ -62,7 +64,7 @@ var svg = d3.select(div).append("svg")
       "translate(" + margin.left + "," + margin.top + ")");
 
 
-var div = d3.select("body").append("div")
+var tooltipDiv = d3.select("body").append("div")
 .attr("class", "tooltip")
 .style("opacity", 0);
       
@@ -90,19 +92,19 @@ y.domain([0, 3]);
       .attr("y", function(d) { return y(d.avgRating); })
       .attr("width", x.bandwidth)
       .attr("height", function(d) { return height - y(d.avgRating); })
-    .on("mouseover", function(d) {
-    div.transition()
-        .duration(200)
-        .style("opacity", .9);
-    div.html("Year: " + d.year + "<br/>" + "Avg rating: " +d.avgRating.toFixed(1)+ "<br/>" + "Count: " +d.count)
-        .style("left", (d3.event.pageX) + "px")
-        .style("top", (d3.event.pageY - 28) + "px");
-    })
-    .on("mouseout", function(d) {
-    div.transition()
-        .duration(500)
-        .style("opacity", 0);
-    });
+        .on("mouseover", function(d) {
+        tooltipDiv.transition()
+            .duration(200)
+            .style("opacity", .9);
+        tooltipDiv.html("Year: " + d.year + "<br/>" + "Avg rating: " +d.avgRating.toFixed(1)+ "<br/>" + "Count: " +d.count)
+            .style("left", (d3.event.pageX) + "px")
+            .style("top", (d3.event.pageY - 28) + "px");
+        })
+        .on("mouseout", function(d) {
+        tooltipDiv.transition()
+            .duration(500)
+            .style("opacity", 0);
+        });
 
 
 // add the x Axis
